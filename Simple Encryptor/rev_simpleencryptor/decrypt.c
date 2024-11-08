abdd9bff4b064c66540b18b56db77233be567da4
Diego-Granados <diegranados@estudiantec.cr>
2024-11-07 21:54:49 -0600

    initial commit

---
+++ Simple Encryptor/rev_simpleencryptor/decrypt.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <time.h>
+#include <string.h>
+
+int main() {
+    FILE *flag = fopen("flag.enc", "rb");
+    if (!flag) {
+        perror("Error al abrir el archivo");
+        return 1;
+    }
+
+    uint32_t seed;
+    fread(&seed, sizeof(seed), 1, flag);
+    printf("%u\n", seed); 
+    printf("%s\n", ctime((time_t *)&seed)); 
+    srand(seed);
+    
+    uint8_t byte;
+    uint8_t decrypted_flag[1024];
+    size_t decrypted_length = 0;
+
+    while (fread(&byte, sizeof(byte), 1, flag) == 1) {
+
+        int rand_num1 = rand();
+        
+        int rand_num2 = rand() & 7;  
+        
+        byte = (byte >> rand_num2) | (byte << (8 - rand_num2) & 0xFF);
+        byte = (byte ^ rand_num1) & 0xFF;
+        
+        decrypted_flag[decrypted_length++] = byte;
+    }
+    
+    fclose(flag);
+
+    decrypted_flag[decrypted_length] = '\0';
+
+    if (decrypted_length > 0 && decrypted_flag[0] != '\0') {
+        printf("%s\n", decrypted_flag);
+    } else {
+        for (size_t i = 0; i < decrypted_length; i++) {
+            printf("%c", decrypted_flag[i]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
